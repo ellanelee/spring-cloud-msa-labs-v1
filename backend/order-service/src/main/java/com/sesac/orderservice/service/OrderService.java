2e0de83698adbd3e95fee66132f87f5fdeda4038
ellanelee <ljedoc@naver.com>
2025-08-12 16:46:02 +0900

    OpenTelemetry 및 zipkin설정

--- backend/order-service/src/main/java/com/sesac/orderservice/service/OrderService.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/service/OrderService.java
@@ -1,13 +1,16 @@
 package com.sesac.orderservice.service;
 
 import com.sesac.orderservice.client.ProductServiceClient;
-import com.sesac.orderservice.client.UserServiceClient;
 import com.sesac.orderservice.client.dto.ProductDto;
 import com.sesac.orderservice.client.dto.UserDto;
 import com.sesac.orderservice.dto.OrderRequestDto;
 import com.sesac.orderservice.entity.Order;
 import com.sesac.orderservice.facade.UserServiceFacade;
 import com.sesac.orderservice.repository.OrderRepository;
+
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +25,8 @@ public class OrderService {
   private final OrderRepository orderRepository;
   private final ProductServiceClient productServiceClient;
   private final UserServiceFacade userServiceFacade;
+  private final Tracer tracer;
+
 
   public Order findById(Long id) {
       return orderRepository.findById(id).orElseThrow(
@@ -32,22 +37,36 @@ public class OrderService {
     @Transactional
     public Order createOrder(OrderRequestDto request) {
 
-        UserDto user = userServiceFacade.getUserById(request.getUserId());
-        if (user == null) throw new RuntimeException("User not found");
+       Span span = tracer.nextSpan()
+               .name("createOrder")
+               .tag("order.userId", request.getUserId())
+               .tag("order.productId", request.getProductId())
+               .start();
 
-        ProductDto product = productServiceClient.getProductById(request.getProductId());
-        if (product == null) throw new RuntimeException("Product not found");
+       try(Tracer.SpanInScope ss = tracer.withSpan(span)) {
 
-        if (product.getStockQuantity() < request.getQuantity()) {
-            throw new RuntimeException("Out of stock");
-        }
+           UserDto user = userServiceFacade.getUserById(request.getUserId());
+           if (user == null) throw new RuntimeException("User not found");
 
-        Order order = new Order();
-        order.setUserId(request.getUserId());
-        order.setTotalAmount(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
-        order.setStatus("COMPLETED");
+           ProductDto product = productServiceClient.getProductById(request.getProductId());
+           if (product == null) throw new RuntimeException("Product not found");
 
-        return orderRepository.save(order);
+           if (product.getStockQuantity() < request.getQuantity()) {
+               throw new RuntimeException("Out of stock");
+           }
+
+           Order order = new Order();
+           order.setUserId(request.getUserId());
+           order.setTotalAmount(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+           order.setStatus("COMPLETED");
+
+           return orderRepository.save(order);
+       }catch(Exception e){
+          span.tag("error", e.getMessage());
+          throw e;
+       }finally {
+          span.end();
+       }
     }
 
     public List<Order> getOrdersByUserId(Long userId) {
