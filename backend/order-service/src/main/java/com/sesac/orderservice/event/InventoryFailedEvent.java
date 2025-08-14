db1c0923ae47eaf5f1936e79d7863dc3cde15f38
ellanelee <ljedoc@naver.com>
2025-08-14 16:42:58 +0900

    feat: order-service에서 이벤트 수신 및 큐 소비 - productService - inventoryFailedEvent - 큐소비 및 로그출력

---
+++ backend/order-service/src/main/java/com/sesac/orderservice/event/InventoryFailedEvent.java
@@ -0,0 +1,18 @@
+package com.sesac.orderservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryFailedEvent {
+    private static final long serialVersionUID = 1L;  //직렬화에 사용할 버전 정보 (스트림 처리)
+
+    private Long orderId;
+    private Long productId;
+    private Integer quantity;
+    private String reason;
+}
--- backend/payment-service/src/main/java/com/sesac/paymentservice/service/PaymentService.java
+++ backend/payment-service/src/main/java/com/sesac/paymentservice/service/PaymentService.java
@@ -1,26 +1,28 @@
 package com.sesac.paymentservice.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.sesac.paymentservice.entity.Payment;
 import com.sesac.paymentservice.entity.PaymentStatus;
 import com.sesac.paymentservice.event.PaymentRequestEvent;
 import com.sesac.paymentservice.repository.PaymentRepository;
-import lombok.Data;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-@Data
 @Slf4j
 public class PaymentService {
-
     private final PaymentRepository paymentRepository;
 
+
+    @Transactional
+    // 결제 로직
     public Payment processPayment(PaymentRequestEvent event) {
-        // 결재 성공과 실패 (random하게 줄수 있고, 조건을 통해 실패를 유발)
+        // 성공, 실패 로직 구현
         Payment payment = new Payment();
         payment.setOrderId(event.getOrderId());
         payment.setUserId(event.getUserId());
@@ -30,19 +32,24 @@ public class PaymentService {
 
         Payment savedPayment = paymentRepository.save(payment);
 
-        try {
-            Thread.sleep(3000);
-            if(Math.random() <0.3) {
+        // 성공을 하던 실패를 하던 결제 처리 시뮬레이션 - 일단 랜덤하게 설정
+        try{
+            Thread.sleep(2000);
+
+            if(Math.random() < 0.3) {
                 throw new RuntimeException("잔액 부족");
             }
-            payment.setStatus(PaymentStatus.COMPLETED);
+
+            savedPayment.setStatus(PaymentStatus.COMPLETED);
             paymentRepository.save(savedPayment);
-        } catch (Exception e) {
+        }catch (Exception e) {
             savedPayment.setStatus(PaymentStatus.FAILED);
             savedPayment.setFailureReason(e.getMessage());
             paymentRepository.save(savedPayment);
-            throw new RuntimeException("결제실패");
+            throw new RuntimeException("결제 실패");
         }
-     return savedPayment;
+        return savedPayment;
     }
+
 }
+
